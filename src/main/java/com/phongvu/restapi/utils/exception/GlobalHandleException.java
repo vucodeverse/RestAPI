d1391be94436a088b17b0eab2279ef240fac6580
vucodeverse <vunguyen111105@gmail.com>
2026-01-26 16:03:56 +0700

    Add global exception handler

--- src/main/java/com/phongvu/restapi/utils/exception/GlobalHandleException.java
+++ src/main/java/com/phongvu/restapi/utils/exception/GlobalHandleException.java
@@ -1,5 +1,6 @@
 package com.phongvu.restapi.utils.exception;
 
+import com.phongvu.restapi.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,13 +11,27 @@ import java.util.Objects;
 @ControllerAdvice
 public class GlobalHandleException {
     @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<String> handlingRuntimeException(RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException e) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(400);
+        apiResponse.setMessage(e.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse> handlingAppException(AppException e) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(e.getErrorCode().getCode());
+        apiResponse.setMessage(e.getErrorCode().getMsg());
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<String> handlingValidation(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(Objects.requireNonNull(e.getFieldError()).getDefaultMessage());
+    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException e) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(400);
+        apiResponse.setMessage(Objects.requireNonNull(e.getFieldError()).getDefaultMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
 }
