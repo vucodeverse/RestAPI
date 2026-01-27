83a97c4a1023e5eb060aba71c1bf972f5bc741d5
vucodeverse <vunguyen111105@gmail.com>
2026-01-27 22:37:59 +0700

    Add new message of exception

--- src/main/java/com/phongvu/restapi/constants/ApiMessage.java
+++ src/main/java/com/phongvu/restapi/constants/ApiMessage.java
@@ -8,7 +8,9 @@ public enum ApiMessage {
     USER_UPDATED(200, "Update user successfully"),
     USER_DELETED(204, "Delete user successfully"),
     GET_USER_BY_ID(200, "Get user successfully"),
-    GET_ALL_USER(200, "Get all users successfully")
+    GET_ALL_USER(200, "Get all users successfully"),
+    UNAUTHENTICATED(200, "Unauthenticated"),
+    INTERNAL_SERVER_ERROR(200, "ERROR SERVER")
     ;
 
     ApiMessage(int code, String msg) {
