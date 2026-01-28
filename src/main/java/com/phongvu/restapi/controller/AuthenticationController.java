ee4fbda712ec14a4d241f9511b6d593ef1afa561
vucodeverse <vunguyen111105@gmail.com>
2026-01-28 14:53:40 +0700

    Add new function to introspect token

--- src/main/java/com/phongvu/restapi/controller/AuthenticationController.java
+++ src/main/java/com/phongvu/restapi/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.phongvu.restapi.controller;
 
+import com.nimbusds.jose.JOSEException;
 import com.phongvu.restapi.dto.request.ApiResponse;
 import com.phongvu.restapi.dto.request.AuthenticationRequest;
+import com.phongvu.restapi.dto.request.IntrospectRequest;
 import com.phongvu.restapi.dto.response.AuthenticationResponse;
+import com.phongvu.restapi.dto.response.IntrospectResponse;
 import com.phongvu.restapi.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -32,7 +37,29 @@ public class AuthenticationController {
         var authenticationResult = authenticationService.authenticate(request);
 
         ApiResponse<AuthenticationResponse> response = new ApiResponse<>();
+        response.setCode(200);
         response.setResult(authenticationResult);
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Introspect JWT token to check its validity.
+     *
+     * <p>This endpoint receives a JWT token from client,
+     * delegates validation logic to {@link AuthenticationService},
+     * and returns whether the token is valid or not.</p>
+     *
+     * @param request the introspection request containing JWT token
+     * @return HTTP 200 response with token validity information
+     */
+    @PostMapping(path = "introspect")
+    ResponseEntity<ApiResponse<IntrospectResponse>> introspect(@RequestBody IntrospectRequest request) {
+        var result = authenticationService.introspect(request);
+
+        ApiResponse<IntrospectResponse> response = new ApiResponse<>();
+        response.setCode(200);
+        response.setResult(result);
 
         return ResponseEntity.ok(response);
     }
